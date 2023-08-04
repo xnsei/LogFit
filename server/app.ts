02f8ccf67883413e7bd626243d9c6a0879ec7eda
xnsei <banagetanmay@gmail.com>
2023-08-04 17:15:36 +0530

    removing routes files

--- server/app.ts
+++ server/app.ts
@@ -2,31 +2,48 @@ import connectDB from "./dbConnection";
 
 const express = require("express");
 const cors = require("cors");
-const workouts = require("./routes/workouts");
-const exercises = require("./routes/exercises");
-
-// const workout = require("./models/workout");
-
 const app = express();
+const workout = require("./models/workout");
+const wrapAssync = require("./utils/wrapAssync");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/workouts/", workouts);
-app.use("/exercises", exercises);
-
 connectDB();
+
+app.get(
+  "/",
+  wrapAssync(async (req: any, res: any) => {
+    const workouts = await workout.find({});
+    res.send(workouts);
+  })
+);
+
+app.post(
+  "/new",
+  wrapAssync(async (req: any, res: any) => {
+    const { name } = req.body;
+    const newWorkout = new workout({
+      name,
+    });
+    await newWorkout.save();
+    res.json({ message: "Workout Saved SuccessFully!!" });
+  })
+);
+
+app.get(
+  "/:id",
+  wrapAssync(async (req: any, res: any) => {
+    const { id } = req.params;
+    const requestedWorkout = await workout.findById(id);
+    res.json(requestedWorkout);
+  })
+);
 
 app.get("/", (req: any, res: any) => {
   res.json({ message: "Hello World!!!" });
 });
-
-// app.get("/workouts/:id", async (req: any, res: any) => {
-//   const { id } = req.params;
-//   const requestedWorkout = await workout.findById(id);
-//   res.json(requestedWorkout);
-// });
 
 app.listen(8000, () => {
   console.log("server is running on port: 8000");
