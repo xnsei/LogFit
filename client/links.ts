4828d92a74f358850d8c4d6dc7d3d6d01d142586
Tanmay Banage <banagetanmay@gmail.com>
2023-09-03 18:01:01 +0530

    using vite env variable

--- client/links.ts
+++ client/links.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const baseURL = process.env.BASEURL || "http://localhost:8000";
+const baseURL =
+  JSON.stringify(import.meta.env.VITE_BASEURL) || "http://localhost:8000";
 
 export default baseURL;
